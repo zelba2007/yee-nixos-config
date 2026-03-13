d05590fa635add917271f800a1f55697c2580a89
zelba2007 <59406681+zelba2007@users.noreply.github.com>
2026-03-13 16:05:44 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "Cook configuration";
-  nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -19,12 +10,6 @@
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-
-    # CookNxivim 配置完整版
-    CookNixvim = {
-      url = "github:Youthdreamer/CookNixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     catppuccin.url = "github:catppuccin/nix/release-25.11";
@@ -79,9 +64,6 @@
                 agenix.homeManagerModules.default
                 noctalia.homeModules.default
                 ./home/youth
-              ];
-              home.packages = [
-                CookNixvim.packages.${system}.default
               ];
             };
           }
